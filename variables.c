d2e66fd1398b940486e3f98c74a1b7037a922caf
rahul1387 <rahultripathi7447@gmail.com>
2025-09-29 22:12:53 +0530

    Variables in C.

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 3;      // Integer variable
+    float num2 = 4.5;  // Floating-point variable
+    char letter = 'A'; // Character variable
+
+    return 0;
+}
