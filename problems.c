d5ad1eb149f9dd63a53d3a75120259de1599eebb
rahul1387 <rahultripathi7447@gmail.com>
2025-10-01 22:01:22 +0530

    Problems using C.

---
+++ problems.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// area of squeare
+int main()
+{
+    int side;
+    printf("Enter side");
+    scanf("%d", &side);
+
+    printf("The area of square is: ", side * side);
+    return 0;
+}
