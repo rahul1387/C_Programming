ec9711150e9bc46a1761271bad4b618248099ab9
rahul1387 <rahultripathi7447@gmail.com>
2025-10-05 20:21:37 +0530

    instructions in C.

--- instructions.c
+++ instructions.c
@@ -9,6 +9,6 @@ int main()
     int c = b + 1;
     int d = 1, e;
     int power = pow(a, c);
-    printf("%d", power);
+    printf("%d", power); // This will print power of a and c.
     return 0;
 }
