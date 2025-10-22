4628ef50619220eb2827b9168e4c34aad0a123ca
rahul1387 <rahultripathi7447@gmail.com>
2025-10-22 20:43:44 +0530

    conditionals and ternary operations.

--- conditionals.c
+++ conditionals.c
@@ -6,16 +6,32 @@ int main()
     printf("Enter your age: ");
     scanf("%d", &age);
 
+    // if-else and if-else ladder.
     if (age >= 18)
     {
-        printf("You can drive!!");
+        printf("You can drive!! \n");
     }
     else if (age < 18)
     {
-        printf("You cannot drive :(");
+        printf("You cannot drive :( \n");
     }
     else
     {
-        printf("Invalid age");
+        printf("Invalid age \n");
     }
+
+    // Ternary operations --> Are used to simplify the code instead of using if else we can directly use this.
+
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    number > 0 ? printf("Number is positive") : number < 0 ? printf("Number is negative") : printf("Number is zero");
+
+    /*
+    syntax to use ternary operations is :
+
+        1. condition ? do Something if TRUE : do Something if FALSE; ---> This is like using only if else.
+        2. Condition ? do Something if TRUE : condition ? do Something if TRUE : do Something if FALSE ---> This is like using if-else ladder.
+    */
 }
