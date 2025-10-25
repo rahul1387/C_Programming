134d4b227f59456352a3d97639ff346b4d201df1
Rahul Tripathi <129259559+rahul1387@users.noreply.github.com>
2025-10-25 17:59:11 +0530

    Switch Statement in C.

--- switchStatement.c
+++ switchStatement.c
@@ -14,7 +14,7 @@ int main(){
         printf("Tuesday");
         break;
     case 3:
-        printf("Wednesday");
+        printf("Wednesday"); 
         break;
     case 4:
         printf("Thursday");
