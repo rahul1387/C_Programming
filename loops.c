153207b051e2f5a35e696f9df1b6e49460261617
rahul1387 <rahultripathi7447@gmail.com>
2025-10-25 18:38:13 +0530

    Loops in C.

---
+++ loops.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+// Loops are used to repeat some parts of program.
+// Types of Loops: 1. for loop ---> used to repeat's the program for specific times.
+
+for(int i = 0; i<=10; i++){
+    printf(i);
+}
