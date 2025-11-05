2832fbfa776b2169acd3742daf53d961c4628d47
rahul1387 <rahultripathi7447@gmail.com>
2025-11-05 22:43:30 +0530

    Problems in C.

---
+++ passwordStrength.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    char arrPass[15];
+    printf("Enter your password: ");
+    scanf("%c" , &arrPass);
+
+    printf(" %c" , arrPass);
+    
+}
