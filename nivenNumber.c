2fae9b49f90d0a6a0ec6e29e881038c531ed914d
rahul1387 <rahultripathi7447@gmail.com>
2025-11-07 19:46:58 +0530

    Niven Number finding in C.

--- nivenNumber.c
+++ nivenNumber.c
@@ -1,19 +1,23 @@
 #include<stdio.h>
-#include<conio.h>
-
-int main(){
-    int number,sum;
-    printf("Enter any number: ");
-    scanf("%d", &number);
-
-    sum += number;
-    if (sum % number == 0)
+void main()
+{
+    int num1,num2,digit,sum=0;
+    printf("Enter you number:");
+    scanf("%d",&num1);
+    num2=num1;
+    while(num1!=0)
     {
-        printf("The number is niven");
+        digit=num1%10;
+        sum+=digit;
+        num1=num1/10;
     }
-    else{
-        printf("The number is not niven number");
+    if(num2%sum==0)
+    {
+        printf("The number %d is a niven number",num2);
     }
-    // Niven Number
-    // Using if-else and basic
+    else
+    {
+        printf("The number %d is not a nivem number",num2);
+    }
+
 }
